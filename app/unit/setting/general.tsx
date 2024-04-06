2d236240e1764b6489922ed62b4669d8338b6086
acronymor <acronymor@gmail.com>
2024-04-06 23:56:59 +0800

    [FIX] fix error that can't be reloaded when theme changed

--- app/unit/setting/general.tsx
+++ app/unit/setting/general.tsx
@@ -5,9 +5,17 @@ import {IconButton} from "@/app/ui/lib/button";
 import {Select} from "@/app/ui/lib/select";
 import {SubmitKey, Theme} from "@/app/proto/setting";
 import {getGenericSetting, setGenericSetting} from "@/app/unit/setting/lib/data";
+import {revalidatePath} from "next/cache";
 
 export async function General() {
     const config = await getGenericSetting()
+
+    const update = async (key: string, value: any) => {
+        'use server'
+
+        await setGenericSetting(key, value)
+        revalidatePath('/unit/setting');
+    }
 
     return (
         <List>
@@ -20,7 +28,7 @@ export async function General() {
             </ListItem>
 
             <ListItem title={"发送键"}>
-                <Select uKey={"submitKey"} uValue={config["submitKey"]} update={setGenericSetting}>
+                <Select uKey={"submitKey"} uValue={config["submitKey"]} update={update}>
                     {
                         Object.values(SubmitKey).map((v) => (
                             <option value={v} key={v}>
@@ -32,7 +40,7 @@ export async function General() {
             </ListItem>
 
             <ListItem title={"主题"}>
-                <Select uKey={"theme"} uValue={config["theme"]} update={setGenericSetting}>
+                <Select uKey={"theme"} uValue={config["theme"]} update={update}>
                     {
                         Object.values(Theme).map((v) => (
                             <option value={v} key={v}>
