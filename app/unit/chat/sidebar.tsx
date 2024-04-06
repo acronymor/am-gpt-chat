048494a693b9da19ca1de900f3856d3873948b88
acronymor <acronymor@gmail.com>
2024-04-06 18:33:50 +0800

    [FIX] fix mask and plugin icon

--- app/unit/chat/sidebar.tsx
+++ app/unit/chat/sidebar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React from "react";
+import {useRouter} from "next/navigation";
 
 import {IconButton} from "@/app/ui/lib/button";
 import {IconLink} from "@/app/ui/lib/link";
@@ -17,9 +18,12 @@ import AddIcon from "@/app/icons/add.svg";
 
 export function Sidebar(props: { className?: string }) {
     const store = useChatStore()
+    const router = useRouter()
+
+
     return (
         <div className={sidebar_style["sidebar"]}>
-            <IconLink href={{pathname: "/unit"}}>
+            <IconLink href={{pathname: "/unit/chat"}}>
                 <div className={sidebar_style["sidebar-header"]}>
                     <div className={sidebar_style["sidebar-title"]}>NextChat</div>
                     <div className={sidebar_style["sidebar-sub-title"]}>Build your own AI assistant.</div>
@@ -28,26 +32,22 @@ export function Sidebar(props: { className?: string }) {
             </IconLink>
 
             <div className={sidebar_style["sidebar-header-bar"]}>
-                <IconLink href={{pathname: `/unit/mask`}}>
-                    <IconButton
-                        className={sidebar_style["sidebar-bar-button"]}
-                        icon={<MaskIcon/>}
-                        text={"Mask"}
-                        onClick={() => {
-                            console.log("mask")
-                        }}
-                    />
-                </IconLink>
-                <IconLink href={{pathname: `/unit/mask`}}>
-                    <IconButton
-                        className={sidebar_style["sidebar-bar-button"]}
-                        icon={<PluginIcon/>}
-                        text={"Plugin"}
-                        onClick={() => {
-                            console.log("插件")
-                        }}
-                    />
-                </IconLink>
+                <IconButton
+                    className={sidebar_style["sidebar-bar-button"]}
+                    icon={<MaskIcon/>}
+                    text={"Mask"}
+                    onClick={() => {
+                        router.push('/unit/mask')
+                    }}
+                />
+                <IconButton
+                    className={sidebar_style["sidebar-bar-button"]}
+                    icon={<PluginIcon/>}
+                    text={"Plugin"}
+                    onClick={() => {
+                        router.push('/unit/mask')
+                    }}
+                />
             </div>
 
             <div className={sidebar_style["sidebar-body"]}>
